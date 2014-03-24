16934fc6c93e2f7d794e673a821fa6ec8902ecad
▟ ▖▟ ▖ <dodo.the.last@gmail.com>
2014-03-24 22:02:03 +0100

    update host input field placeholder properly

--- extension/options.js
+++ extension/options.js
@@ -43,10 +43,9 @@ document.addEventListener('DOMContentLoaded', function restore() {
 document.getElementById('jid').addEventListener('keyup', function keyup(ev) {
     var host = document.getElementById('host');
     var value = this.value.trim();
-    if (host.value.trim().length) return;
     var i = value.lastIndexOf("@");
     if (i > -1) host.placeholder = value.substr(i + 1);
-    else host.placeholder = "localhost";
+    else host.placeholder = localStorage['host'] || "localhost";
 });
 
 document.getElementById('save-jid').addEventListener('click', function save(ev) {
@@ -65,10 +64,13 @@ document.getElementById('save-jid-params').addEventListener('click', function sa
         if (el.value.trim().length) localStorage[id] = el.value.trim();
         else delete localStorage[id];
     });
-    if (!localStorage['host'] && localStorage['jid']) {
+    if (localStorage['host'] && localStorage['jid']) {
         var jid = localStorage['jid'];
         var i = jid.lastIndexOf("@");
-        if (i > -1) localStorage['host'] = jid.substr(i + 1);
+        if (i > -1 && jid.substr(i + 1) === localStorage['host']) {
+            delete localStorage['host'];
+            host.value = "";
+        }
     }
     el = document.getElementById('reconnect');
     if (el.checked) localStorage['reconnect'] = 'on';
